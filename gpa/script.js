e5e65a09f4fe055dba5eacbf7e6bb65121411538
Omar <55400535+Omar149@users.noreply.github.com>
2026-01-30 17:50:27 +0200

    Add files via upload

---
+++ gpa/script.js
@@ -0,0 +1,50 @@
+const addButton = document.getElementById("addCourse");
+const deleteButton = document.getElementById("deleteCourse");
+const calcButton = document.getElementById("calculate");
+const tableBody = document.getElementById("tbody");
+const result = document.getElementById("result");
+let i = 1;
+
+function addC(){
+    i++;
+    let row = tableBody.insertRow();
+    row.insertAdjacentHTML('beforeend', `<td>${i}</td><td><input type="text" class="form-control form-control-sm bg-dark text-light border-primary text-center" placeholder="Course name (optional)"></td><td><input type="number" class="ch form-control form-control-sm bg-dark text-light border-primary text-center" placeholder="Credit Hours"></td><td><input type="number" class="gpa form-control form-control-sm bg-dark text-light border-primary text-center" placeholder="GPA"></td>`);
+}
+function deleteC(){
+    if(i===1){return}
+    else {
+        i--;
+        tableBody.deleteRow(i);
+    }
+}
+function calculate(){
+    let gpas = document.querySelectorAll('.gpa');
+    let ch = document.querySelectorAll('.ch');
+    for(let e = 0; e<i; e++){
+        let val = Number(gpas[e].value);
+        let val2 = Number(ch[e].value);
+        if(!gpas[e].value.trim || isNaN(val) || val<=0){
+            alert(`Please enter your GPAs (Row ${e+1})!`);
+            return;
+        }
+        if(!ch[e].value.trim || isNaN(val2) || val2<=0){
+            alert(`Please enter your Credit Hours (Row ${e+1})!`);
+            return;
+        }
+    }
+    let sum = [];
+    let chArray = [];
+    for(let e = 0; e<i; e++){
+        sum[e] = gpas[e].value*ch[e].value;
+        chArray[e] = ch[e].value;
+    }
+    sum = sum.reduce((a,b)=> a + b, 0);
+    chArray = chArray.map(Number);
+    chArray = chArray.reduce((a,b)=> a + b, 0);
+    let gpa = sum/chArray;
+    result.innerText = `Final GPA: ${gpa.toFixed(2)}`;
+}
+
+addButton.addEventListener("click", addC);
+deleteButton.addEventListener("click", deleteC);
+calcButton.addEventListener("click", calculate);
